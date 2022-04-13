f616665587e91b73b9c3c92bf1882d9e6b688e42
Sergey Okun <okun79@gmail.com>
2022-04-13 10:38:14 +0300

    added main and: printThreeWords checkSumSign

--- src/main/java/lesson01/HomeWorkApp.java
+++ src/main/java/lesson01/HomeWorkApp.java
@@ -1,4 +1,28 @@
 package lesson01;
 
 public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+    }
+
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+    private static void checkSumSign() {
+        int a = 3;
+        int b = 7;
+        int result = a + b;
+        if(result >= 0 ) {
+            System.out.println("Сумма положительная");
+        } else
+            System.out.println("Сумма отрицательная");
+    }
+
+
 }
