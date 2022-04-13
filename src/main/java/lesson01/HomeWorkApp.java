b340ac109b03b81b36de59b31679cb41c258b650
Sergey Okun <okun79@gmail.com>
2022-04-13 17:07:49 +0300

    Revert "added methods:"
    This reverts commit 69b3041a8045eeec8fae13a21caf54726960eac9.

--- src/main/java/lesson01/HomeWorkApp.java
+++ src/main/java/lesson01/HomeWorkApp.java
@@ -4,8 +4,6 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
-        printColor();
-        compareNumbers();
     }
 
 
@@ -18,30 +16,12 @@ public class HomeWorkApp {
 
     private static void checkSumSign() {
         int a = 3;
-        int b = -3;
+        int b = 7;
         int result = a + b;
         if(result >= 0 ) {
             System.out.println("Сумма положительная");
         } else
             System.out.println("Сумма отрицательная");
-    }
-
-
-    private static void printColor() {
-        int value = 100;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
-        } else System.out.println("Зелёный");
-    }
-
-    private static void compareNumbers() {
-        int a = 13;
-        int b = 47;
-        if (a >= b) {
-            System.out.println("a >= b");
-        } else System.out.println("a < b");
     }
 
 
